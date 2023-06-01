3218f16fda85e8a190f96b4e0e57cfc4a848eced
dima tkachenko <dtkachenko06@gmail.com>
2023-06-01 13:11:12 +0300

    make project Fibonachi

---
+++ project 1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace project_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int series = 40;
+            int sumNow = 0;
+            int sumPrev = 1;
+            int sumNext= 0;
+            for(int i = 0; i < series; i++)
+            {
+                sumNext = sumNow + sumPrev;
+                Console.WriteLine(sumNext);
+                sumPrev = sumNow;
+                sumNow= sumNext;
+            }
+
+
+        }
+    }
+}
